194c6e6a2cb4f248b032f7c1873cd25923d13465
Alianhakim <alianhakim8@gmail.com>
2020-11-28 11:22:05 +0700

    applying MVC Architecture

---
+++ src/main/java/com/alian/Spring/Boot/Web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.alian.Spring.Boot.Web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+}
